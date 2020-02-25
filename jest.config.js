d2f2d3e6bee2afd6f66104edbab2a2293b7e225d
Urban Pettersson <urban.pettersson@enfogroup.com>
2020-02-25 19:08:52 +0100

    chore(configuration): add configuration for all development tools used
    Added configuration for eslint, commitlint, jest, tsconfig and webpack

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  preset: 'ts-jest',
+  testPathIgnorePatterns: ['/node_modules/'],
+  testRegex: '(/tests/.*|(\\.|/)(test|spec))\\.[jt]sx?$',
+  moduleFileExtensions: ['ts', 'js'],
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
+  },
+  collectCoverageFrom: [
+    'src/**/*.ts',
+  ],
+  verbose: true
+};
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  devtool: 'source-map',
+  entry: {
+    index: './src/index.ts'
+  },
+  mode: 'production',
+  resolve: {
+    symlinks: false,
+    extensions: ['.js', '.json', '.ts']
+  },
+  output: {
+    libraryTarget: 'commonjs2',
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  target: 'node',
+  module: {
+    rules: [{ test: /\.ts(x?)$/, include: path.resolve(__dirname, 'src'), loader: 'ts-loader' }]
+  }
+};
